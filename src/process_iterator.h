757f01aa0209996723a4fcff47ac9ab217404d15
ajsiu <ajs38@iu.edu>
2024-02-22 10:47:22 -0500

    Revert "Reworking usage calculations after test fails" Better method worked out in different branch
    This reverts commit 190659f3c8d02aa9b57c34713f8d80d3679451f3.

--- src/process_iterator.h
+++ src/process_iterator.h
@@ -49,10 +49,6 @@
 #include <kvm.h>
 #endif
 
-#ifdef __APPLE__
-#include <mach/task.h>
-#endif
-
 // process descriptor
 struct process {
 	//pid of the process
@@ -65,10 +61,6 @@ struct process {
 	int cputime;
 	//actual cpu usage estimation (value in range 0-1)
 	double cpu_usage;
-#ifdef __APPLE__
-    struct timeval total_time;
-    task_t task;//remeber to init as MACH_PORT_NULL
-#endif
 	//absolute path of the executable file
 	char command[PATH_MAX+1];
 };
